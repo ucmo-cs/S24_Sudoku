5a7079466ae37c4553f239377abb0710588e7642
drn01050 <114438876+drn01050@users.noreply.github.com>
2024-03-21 12:46:45 -0500

    Update HelloApplication.java

--- src/main/java/gui/sudokuteacher/scenes/HelloApplication.java
+++ src/main/java/gui/sudokuteacher/scenes/HelloApplication.java
@@ -1,10 +1,23 @@
 package gui.sudokuteacher.scenes;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import gui.sudokuteacher.controllers.SudokuBoardController;
 import gui.sudokuteacher.views.ButtonView;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
@@ -18,33 +31,35 @@ public class HelloApplication extends Application {
     SudokuBoardController sudokuController;
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage primaryStage) {
+        Scene mainMenuScene = MainMenuScene.createMainMenu(primaryStage);
 
+        primaryStage.setScene(mainMenuScene);
+        primaryStage.setTitle("Sudoku Teacher");
+        primaryStage.show();
+    }
 
-        SudokuBoardController sudoku = new SudokuBoardController("400000938732094100895310240370609004529001673604703090957008300103900400240030709");
+    public static void startSudokuGame(Stage primaryStage) {
+        try {
+            // Create the Sudoku game scene
+            SudokuBoardController sudoku = new SudokuBoardController("400000938732094100895310240370609004529001673604703090957008300103900400240030709");
+            BorderPane root = new BorderPane();
+            root.setBottom(ButtonView.createButtonBar());
+            root.setPadding(new Insets(10));
+            root.setCenter(sudoku.getSudokuBoardView());
+            root.getCenter().autosize();
+            root.setRight(ButtonView.createNumberPad(sudoku));
 
-        sudokuController = sudoku;
-        BorderPane root = new BorderPane();
+            Scene sudokuGameScene = new Scene(root, 750, 600);
+            sudokuGameScene.setOnKeyPressed(e -> sudoku.keyPressed(e.getCode()));
 
-        root.setBottom(ButtonView.createButtonBar());
-        root.setPadding(new Insets(10)); // Adjust padding as needed
-        //root.setMargin(sudoku.getSudokuBoardView(), new Insets(10)); // Adjust margins as needed
-
-
-        root.setCenter(sudoku.getSudokuBoardView());
-        root.getCenter().autosize();
-        root.setRight(ButtonView.createNumberPad(sudokuController));
-
-
-        Scene scene = new Scene(root, 750, 600);
-
-        //TODO: fix the setOnKeyPress issue, this runs all mouse clicks through board logic which wont work for the application later
-        //scene.setOnKeyPressed(sudoku.getSudokuBoardView().getOnKeyPressed());
-        scene.setOnKeyPressed(this::onKeyPressed);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-
-        stage.show();
+            // Switch to the Sudoku game scene
+            primaryStage.setScene(sudokuGameScene);
+            primaryStage.setTitle("Sudoku Game");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void onKeyPressed(KeyEvent e){
